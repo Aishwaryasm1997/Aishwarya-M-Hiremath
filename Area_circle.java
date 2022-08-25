787ffb80794088d383240ad88ce097171040ffce
Aishwaryasm1997 <110146436+Aishwaryasm1997@users.noreply.github.com>
2022-08-25 11:59:15 +0530

    Add files via upload

---
+++ Area_circle.java
@@ -0,0 +1,19 @@
+package programs.java;
+
+public class Area_circle {
+void area() {
+	//pi value is 3.142.
+	double pi=3.142;
+	// i will give radius value r=5.
+	int r=5;
+	//FOrmula for Area of circle is pir^2
+	double d=pi*r*r;
+	System.out.println(d);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Area_circle var= new Area_circle();
+var.area();
+	}
+
+}
---
+++ Pgm2.java
@@ -0,0 +1,27 @@
+package programs.java;
+
+public class Pgm2 {
+	//method declaration
+static void calculation() {
+	double  d1 =1.5;
+	double d2=2.5;
+	double sum=d1+d2;
+	double sub=d2-d1;
+	double mul=d1*d2;
+	double div=d2/d1;
+	double mod=d1%d2;
+	System.out.println(sum);
+	System.out.println(sub);
+	System.out.println(mul);
+	System.out.println(div);
+	System.out.println(mod);
+	return;
+	
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//print statement for method calculation
+		calculation();
+	}
+
+}
